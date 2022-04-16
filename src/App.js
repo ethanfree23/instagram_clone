9ba7a7b47f7dabe50fa3a31a0a0cdf148f198c04
Ethan Freeman <ethanfree23@gmail.com>
2022-04-16 13:42:36 -0500

    cleaner

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import Post from './Post.js'
-import { db } from './firebase'
+import { db, auth } from './firebase'
 import { makeStyles } from '@material-ui/core/styles';
 import Modal from '@material-ui/core/Modal';
+import Input from '@material-ui/core/Input';
 import { Button } from '@mui/material';
 
-// 1:36:15hr youtube video, when i started looking for how to start firebase from console to render on the page
+
 
 function getModalStyle() {
   const top = 50;
@@ -37,6 +38,10 @@ function App() {
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
 
+  const [username, setUsername] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [password, setPassword] = useState([]);
+
   // useEffect => runs a piece of code based on a specific condition
   useEffect(() => {
     // "do this when the page loads, and not again... unless "posts" changes" is what useEffect is telling the computer
@@ -50,7 +55,10 @@ function App() {
   }, [posts]);
 
   const signUp = (e) => {
+    e.preventDefault();
 
+    auth.createUserWithEmailAndPassword(email, password)
+    .catch((error) => alert(error.message))
   }
   return (
     <div className="app">
@@ -61,7 +69,34 @@ function App() {
         aria-describedby="modal-modal-description"
       >
         <div style={modalStyle} className={classes.paper}>
-          <h2>I am a modal</h2>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app_headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.bubbles)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.bubbles)}
+            />
+            <Input
+              placeholder="password"
+              type="text"
+              value={password}
+              onChange={(e) => setPassword(e.bubbles)}
+            />
+          </form>
+          <Button onClick={signUp}>Sign Up</Button>
         </div>
       </Modal>
       <div className="app_header">
@@ -71,10 +106,9 @@ function App() {
           alt=""
         />
       </div>
-
       <Button onClick={() => setOpen(true)}>SignUp</Button>
 
-      <h1>This is the start of something great! suck my ass! 😀</h1>
+      <h1>This is the start of something great!😀</h1>
       {
         posts.map(({ id, post }) => (
           <Post
@@ -85,8 +119,6 @@ function App() {
           />
         ))
       }
-
-      {/* Posts */}
 
       {/* Footer */}
     </div>
